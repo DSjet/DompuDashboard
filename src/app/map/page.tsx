6e18802d854ca19561e2bce37dadbbc09ea225b9
DSjet <teddyantonius7@gmail.com>
2024-05-28 16:33:23 +0700

    add map

---
+++ src/app/map/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import * as React from 'react';
+import Map from 'react-map-gl';
+
+function App() {
+  return (
+    <div>
+    <Map
+      mapboxAccessToken="mapbox token"
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14
+      }}
+      style={{width: 600, height: 400}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
+    </div>
+  );
+}
+
+export default App;
